17bd5cde8a027e238082d4c9c402b3c4edc288d2
yahoopuyo <yahoopuyo@gmail.com>
2020-06-20 21:47:35 +0900

    second method for threshold(done), change current value (ongoing)

--- thesis_exp1/thesis_exp1.cpp
+++ thesis_exp1/thesis_exp1.cpp
@@ -17,6 +17,9 @@
 #include <math.h>
 #include <conio.h>
 #include <time.h>
+#include <vector>
+#include <random>
+#include <numeric>
 
 #include "CPUTimer.h"
 #include "CPUTimer.cpp"
@@ -41,84 +44,22 @@ LARGE_INTEGER g_liTmp;
 LARGE_INTEGER g_liEnd;
 
 
+int get_rand( int min_val, int max_val );
 
-void GvsOutPut(char chan, int Value){
+void GvsOutPut(char chan, int Value);
 
- char buffer[5];
- char Send1[8] = {0,0,0,0,0,0,0,0};
- char Send2[8] = {0,0,0,0,0,0,0,0};
- int i;
+void OutPut(int val);
 
- if(chan == '1'){Send1[7]=0;Send1[6]=0;Send1[5]=1;}
-    else if(chan == '2'){Send1[7]=0;Send1[6]=1;Send1[5]=0;}
-    else if(chan == '3'){Send1[7]=0;Send1[6]=1;Send1[5]=1;}
-    else if(chan == '0'){Send1[7]=0;Send1[6]=0;Send1[5]=0;}
- else if(chan == '4'){Send1[7]=1;Send1[6]=0;Send1[5]=0;}
- else if(chan == '5'){Send1[7]=1;Send1[6]=0;Send1[5]=1;}
- else if(chan == '6'){Send1[7]=1;Send1[6]=1;Send1[5]=0;}
+void OutPutP(int val);	//no print
 
+void OutPutTrap(int amp,int pw,int dir);	//台形波チック
 
- if(Value >= 0) Send1[4] = 1;
- else Value = Value * (-1);
-
-   /* 10進数を2進数に変換 */
- for(i=0;i<=7;i++){
-  Send2[i] = Value % 2;
-  Value = Value / 2; 
- }
- for(i=0;i<=3;i++){
-  Send1[i] = Value % 2;
-  Value = Value / 2; 
- }
- buffer[0] = 71;  
- buffer[1] = (char) (Send1[7]*128 + Send1[6]*64 + Send1[5]*32 + Send1[4]*16 + Send1[3]*8 + Send1[2]*4 + Send1[1]*2 + Send1[0]);
- buffer[2] = (char) (Send2[7]*128 + Send2[6]*64 + Send2[5]*32 + Send2[4]*16 + Send2[3]*8 + Send2[2]*4 + Send2[1]*2 + Send2[0]);
- //buffer[0] = 71;
- //buffer[1] = 81;
- 
- hCom->transmit( (BYTE*)buffer, 3);
- Sleep(1);
-
-}
-
-
-
-
-void OutPut(int val)
-{
-		GvsOutPut('1',val);
-		printf("%d\n",val);
-		/*GvsOutPut('1',val);
-		GvsOutPut('2',val);
-		GvsOutPut('3',val);
-		GvsOutPut('4',val);
-		GvsOutPut('5',val);*/
-}
-
-void OutPutP(int val)	//no print
-{
-	GvsOutPut('1',val);	
-}
-
-void OutPutTrap(int amp,int pw,int dir)	//台形波チック
-{
-	int val = 0;
-	while(val < amp){
-		val = val + 100;
-		if(val > amp) val = amp;
-		OutPutP(val*dir);
-		}
-	OutPut(val*dir);
-	myTimer.Start();
-	myTimer.Wait(pw);
-	OutPut(0);
-}
+void shuffle( int* array, int size );	//配列のシャッフル
 
 
 int PEST()	//閾値求める１
 {
 	printf("PEST mode\n");
-	int Count = 0;
 	double i_r_l=0; //bottom limit
 	int point = 500;	//starts from 500
 	int pre_point=0;
@@ -130,7 +71,6 @@ int PEST()	//閾値求める１
 
 	while (tol > threshold)
 	{
-		Count++;
 		if( _kbhit() ){
 			pp = _getch();
 		}
@@ -186,7 +126,116 @@ int PEST()	//閾値求める１
 	return int(i_r_l);
 }
 
+int threshold2()
+{
+	printf("threshold mode\n");
+	int lowerbound=500; //bottom limit
+	int upperbound=3000;
+	int tol = 300;	//tolerance starts from 600
+	int threshold_inc=0;
+	int threshold_dec=0;
+	int pw = 2000000;
+	char pp=0;
 
+	int point = lowerbound;
+
+	printf("upper threshold\n");
+	while (1)	//increase
+	{
+		if( _kbhit() ){
+			pp = _getch();
+		}
+		
+		if (pp == 'n'){
+			printf("%d\n",point);
+			point += tol;
+		}
+		if(pp == 'y'){
+			threshold_inc=point;
+			printf("upper threshold=%d\n",point);
+			break;
+		}
+		if(pp == 'q'){	//escape reset
+			printf("keyboard interruption at point = %d\n",point);
+			OutPut(0);
+			break;
+		}
+		if(pp == 't'){	//
+			int val = 0;
+			while(val < point){
+				val = val + 100;
+				if(val > point) val = point;
+				OutPut(val);
+				}
+			myTimer.Start();
+			myTimer.Wait(pw);
+			OutPut(0);
+		}
+		if(pp == 'Y'){	//for debug, opposite
+			int val = 0;
+			while(val < point){
+				val = val + 100;
+				if(val > point) val = point;
+				OutPut(-val);
+				}
+			myTimer.Start();
+			myTimer.Wait(pw);
+			OutPut(0);
+		}
+		pp = 0;
+	}
+	
+	printf("lower threshold\n");
+	point=upperbound;
+	pp=0;
+	while (1)	//decrease
+	{
+		if( _kbhit() ){
+			pp = _getch();
+		}
+		
+		if (pp == 'n'){
+			threshold_dec=point;
+			printf("lower threshold=%d\n",point);
+			break;
+		}
+		if(pp == 'y'){
+			printf("%d\n",point);
+			point -= tol;
+		}
+		if(pp == 'q'){	//escape reset
+			printf("keyboard interruption at point = %d\n",point);
+			OutPut(0);
+			break;
+		}
+		if(pp == 't'){	//
+			int val = 0;
+			while(val < point){
+				val = val + 100;
+				if(val > point) val = point;
+				OutPut(val);
+				}
+			myTimer.Start();
+			myTimer.Wait(pw);
+			OutPut(0);
+		}
+		if(pp == 'Y'){	//for debug, opposite
+			int val = 0;
+			while(val < point){
+				val = val + 100;
+				if(val > point) val = point;
+				OutPut(-val);
+				}
+			myTimer.Start();
+			myTimer.Wait(pw);
+			OutPut(0);
+		}
+		pp = 0;
+	}
+
+	printf("upper threshold=%d,lower threshold=%d\n",threshold_inc,threshold_dec);
+	return threshold_dec;
+}
 
 void PWM()	//pwmでためす
 {
@@ -199,13 +248,11 @@ void PWM()	//pwmでためす
 	int amp = 2000;
 	char pp = 0;
 
-
 	while(1){
 		if(_kbhit()){
 			pp = _getch();
 
 			if(pp=='q'){
-
 				OutPut(0);
 				printf("exit pwm mode\n");
 				break;
@@ -372,10 +419,9 @@ void AC()	//色々実験したりするよう
 	}
 }
 
-
-void EXP(int i_r_in)	//閾値に従って、電流値を変える実験
+void EXP2(int i_r_in)	//expiriment flow for specific position
 {
-	printf("Experiment mode\n");
+	printf("Change the current value and record the location of sensation\n\n");
 	int i_r=i_r_in;
 	int val=i_r_in;
 	int two_s = 2000000;
@@ -405,8 +451,69 @@ void EXP(int i_r_in)	//閾値に従って、電流値を変える実験
 			}
 		}
 	}
+	/*
+	//define the order of position
+	int pos[6]={0,1,2,3,4,5};
+	string pos_name[6] = {
+		"ulnar rist",
+		"ulnar elbow",
+		"ulnar armpit",
+		"median rist",
+		"median elbow",
+		"median armpit"
+	};
+	shuffle(pos,6);
+	//for (int i = 0; i < 6; i++) cout << "pos_name[" << i << "]=" << pos_name[pos[i]] << endl;
+	*/
+
+	//define the order and value of current
+
+	//i th value is defined by eq: val = a^i * i_r where a is a constant.
+
+	const int DATA_POINTS = 4;	//number of datapoints for current
+	double a=1.3;
+	int current[DATA_POINTS]={0,1,2,3};
+	int current_val[DATA_POINTS] = {0,0,0,0};
+	string current_name[DATA_POINTS] = {
+		"threshold",
+		"second from threshold",
+		"third from threshold",
+		"largest"
+	};
+
+	shuffle(current,DATA_POINTS);
+	for (int i = 0; i < DATA_POINTS; i++) current_val[i] = (int)i_r * pow(a,i);
+	for (int i = 0; i < DATA_POINTS; i++) cout << current_name[current[i]] << " : " << current_val[current[i]] <<endl;
 
 
+	for (int i=0; i < DATA_POINTS; i++)
+	{
+		bool done = false;
+		while(!done)
+		{
+			if(_kbhit()){
+			switch(_getch())
+			{
+			case 't':
+				OutPutTrap(i_r,two_s,1);
+				break;
+			case 'n':
+				i_r += 10;
+				printf("i_r = %d\n",i_r);
+				break;
+			case 'y':
+				ready=true;
+				printf("%d[mA] will be used as threshold from now on\n",i_r);
+				break;
+			case 'q':
+				printf("exit experiment mode\n");
+				return;
+			default:
+				break;
+			}
+		}
+		}
+	}
 }
 
 
@@ -426,9 +533,112 @@ void main(void)
 			if(mode == '0') AC();
 			if(mode == '1') {i_r = PEST(); printf("i_r = %d\n",i_r);}
 			if(mode == '2') PWM();
-			if(mode == '3') EXP(i_r);
+			if(mode == '3') EXP2(10);
+			if(mode == '4') {i_r = threshold2(); printf("i_r = %d\n",i_r);}
+			
 
 	
 		}
 	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int get_rand( int min_val, int max_val )
+{
+	int num = max_val - min_val;
+	random_device rd;
+	mt19937 mt(rd());
+	return mt() % num + min_val;
+}
+
+void GvsOutPut(char chan, int Value){
+
+ char buffer[5];
+ char Send1[8] = {0,0,0,0,0,0,0,0};
+ char Send2[8] = {0,0,0,0,0,0,0,0};
+ int i;
+
+ if(chan == '1'){Send1[7]=0;Send1[6]=0;Send1[5]=1;}
+    else if(chan == '2'){Send1[7]=0;Send1[6]=1;Send1[5]=0;}
+    else if(chan == '3'){Send1[7]=0;Send1[6]=1;Send1[5]=1;}
+    else if(chan == '0'){Send1[7]=0;Send1[6]=0;Send1[5]=0;}
+ else if(chan == '4'){Send1[7]=1;Send1[6]=0;Send1[5]=0;}
+ else if(chan == '5'){Send1[7]=1;Send1[6]=0;Send1[5]=1;}
+ else if(chan == '6'){Send1[7]=1;Send1[6]=1;Send1[5]=0;}
+
+
+ if(Value >= 0) Send1[4] = 1;
+ else Value = Value * (-1);
+
+   /* 10進数を2進数に変換 */
+ for(i=0;i<=7;i++){
+  Send2[i] = Value % 2;
+  Value = Value / 2; 
+ }
+ for(i=0;i<=3;i++){
+  Send1[i] = Value % 2;
+  Value = Value / 2; 
+ }
+ buffer[0] = 71;  
+ buffer[1] = (char) (Send1[7]*128 + Send1[6]*64 + Send1[5]*32 + Send1[4]*16 + Send1[3]*8 + Send1[2]*4 + Send1[1]*2 + Send1[0]);
+ buffer[2] = (char) (Send2[7]*128 + Send2[6]*64 + Send2[5]*32 + Send2[4]*16 + Send2[3]*8 + Send2[2]*4 + Send2[1]*2 + Send2[0]);
+ //buffer[0] = 71;
+ //buffer[1] = 81;
+ 
+ hCom->transmit( (BYTE*)buffer, 3);
+ Sleep(1);
+
+}
+
+void OutPut(int val)
+{
+		GvsOutPut('1',val);
+		printf("%d\n",val);
+}
+
+void OutPutP(int val)
+{
+		GvsOutPut('1',val);
+}
+
+void OutPutTrap(int amp,int pw,int dir)	//台形波チック
+{
+	int val = 0;
+	while(val < amp){
+		val = val + 100;
+		if(val > amp) val = amp;
+		OutPutP(val*dir);
+		}
+	OutPut(val*dir);
+	myTimer.Start();
+	myTimer.Wait(pw);
+	OutPut(0);
+}
+
+void shuffle( int* array, int size ) {
+    for (int i = 0; i < size; i++) {
+        int r = get_rand( i, size );
+        int tmp = array[i];
+        array[i] = array[r];
+        array[r] = tmp;
+    }
 }
